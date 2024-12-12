44630abd61638b28da40077e532e43248b36b439
whdgur5717 <whdgur57117@gmail.com>
2024-12-12 19:31:41 +0900

    feat: fetch and display images from Supabase in Main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <div>안녕하세요</div>
+import { createClient } from "@/utils/server"
+
+import Image from "next/image"
+
+export default async function Home() {
+  const supabase = await createClient()
+  const { data } = await supabase.from("mp").select("*")
+
+  return (
+    <main>
+      <div>안녕하세요</div>
+      <div>
+        {data?.map(item => {
+          return (
+            <Image
+              width={100}
+              height={100}
+              style={{
+                width: "100px",
+                height: "100px",
+              }}
+              quality={100}
+              key={item.id}
+              alt={item.name}
+              src={item.signed_url || ""}></Image>
+          )
+        })}
+      </div>
+    </main>
+  )
 }
