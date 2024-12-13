2aa8025ec9697cf10e04c0c96223c8f84b44b9db
whdgur5717 <whdgur57117@gmail.com>
2024-12-13 15:29:43 +0900

    feat: add Select component using radix-ui/select

---
+++ src/components/Select.tsx
@@ -0,0 +1,198 @@
+import { defineSlotRecipe } from "@pandacss/dev"
+import * as BaseSelect from "@radix-ui/react-select"
+import type { ComponentProps } from "react"
+
+export const select = defineSlotRecipe({
+  className: "select",
+  description: "Styles for the Select component",
+  slots: [
+    "root",
+    "group",
+    "value",
+    "trigger",
+    "viewport",
+    "content",
+    "label",
+    "item",
+    "itemIndicator",
+    "separator",
+  ],
+  base: {
+    trigger: {
+      display: "flex",
+      h: "10",
+      w: "full",
+      alignItems: "center",
+      justifyContent: "space-between",
+      rounded: "md",
+      border: "input",
+      bg: "transparent",
+      px: "3",
+      py: "2",
+      textStyle: "sm",
+      cursor: "pointer",
+      focusRingOffsetColor: "background",
+
+      _placeholder: {
+        color: "muted.foreground",
+      },
+
+      _focus: {
+        outline: "2px solid transparent",
+        outlineOffset: "2px",
+        focusRingWidth: "2",
+        focusRingColor: "ring",
+        focusRingOffsetWidth: "2",
+      },
+
+      _disabled: {
+        cursor: "not-allowed",
+        opacity: "0.5",
+      },
+    },
+    viewport: {
+      p: "1",
+
+      "&[data-position=popper]": {
+        h: "var(--radix-select-trigger-height)",
+        w: "full",
+        minW: "var(--radix-select-trigger-width)",
+      },
+    },
+    content: {
+      position: "relative",
+      zIndex: 50,
+      minW: "8rem",
+      overflow: "hidden",
+      rounded: "md",
+      border: "base",
+      bg: "popover",
+      color: "popover.foreground",
+      shadow: "md",
+
+      "&[data-state=open]": {
+        animateIn: true,
+        fadeIn: 0,
+        zoomIn: 95,
+      },
+
+      "&[data-state=closed]": {
+        animateOut: true,
+        fadeOut: 0,
+        zoomOut: 95,
+      },
+
+      "&[data-side=top]": {
+        slideInFromBottom: "2",
+      },
+
+      "&[data-side=bottom]": {
+        slideInFromTop: "2",
+      },
+
+      "&[data-side=left]": {
+        slideInFromRight: "2",
+      },
+
+      "&[data-side=right]": {
+        slideInFromLeft: "2",
+      },
+
+      "&[data-position=popper]": {
+        "&[data-side=top]": {
+          translateY: "-1",
+        },
+
+        "&[data-side=bottom]": {
+          translateY: "1",
+        },
+
+        "&[data-side=left]": {
+          translateX: "-1",
+        },
+
+        "&[data-side=right]": {
+          translateX: "1",
+        },
+      },
+    },
+    label: {
+      py: "1.5",
+      pl: "8",
+      pr: "2",
+      textStyle: "sm",
+      fontWeight: "semibold",
+    },
+    item: {
+      position: "relative",
+      display: "flex",
+      cursor: "default",
+      userSelect: "none",
+      alignItems: "center",
+      rounded: "sm",
+      py: "1.5",
+      pl: "8",
+      pr: "2",
+      textStyle: "sm",
+      outline: "2px solid transparent",
+
+      _focus: {
+        bg: "accent",
+        color: "accent.foreground",
+      },
+
+      ["&[data-disabled]"]: {
+        pointerEvents: "none",
+        opacity: "0.5",
+      },
+    },
+    itemIndicator: {
+      position: "absolute",
+      left: "2",
+      display: "flex",
+      h: "3.5",
+      w: "3.5",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    separator: {
+      mx: "-1",
+      my: "1",
+      h: "1px",
+      bg: "muted",
+    },
+  },
+})
+
+export const Select = <T extends string>({
+  value,
+  onValueChange,
+  options,
+}: {
+  value: string
+  onValueChange: (e: T) => void
+  options: T[]
+}) => {
+  return (
+    <BaseSelect.Root
+      value={value}
+      onValueChange={onValueChange}>
+      <BaseSelect.Trigger>
+        <BaseSelect.Value placeholder="지역구 선택">{value}</BaseSelect.Value>
+      </BaseSelect.Trigger>
+      <BaseSelect.Portal>
+        <BaseSelect.Content>
+          <BaseSelect.Viewport>
+            {options.map(data => (
+              <BaseSelect.Item
+                key={data}
+                value={data}>
+                <BaseSelect.ItemText>{data}</BaseSelect.ItemText>
+              </BaseSelect.Item>
+            ))}
+          </BaseSelect.Viewport>
+        </BaseSelect.Content>
+      </BaseSelect.Portal>
+    </BaseSelect.Root>
+  )
+}
